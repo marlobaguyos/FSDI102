04b9a5e39dc8d52aaf5c1228d7e3364e24287f63
Marlo <marlo.baguyos@outlook.com>
2020-01-23 08:17:06 -0500

    added Practice folder

---
+++ Practice/Greeter.java
@@ -0,0 +1,19 @@
+package Practice;
+
+import java.io.Console;
+
+public class Greeter {
+
+  public static void main(String[] args) {
+    Console console = System.console();
+    // TODO:  1. Create a new String named place and assign it a value of your choosing.
+    String place = "Anchorage";
+    // TODO:  2. Using the provided console object, 
+    //           prompt the user "What is your first name?" and store that in a variable
+    String userName = console.readLine("What is your first name: ");
+    // TODO:  3. Print out to the console "Hello <FIRST_NAME>!  Welcome to <PLACE>."
+    console.printf("Hello %s!  Welcome to %s", userName, place);
+  }
+  
+
+}
---
+++ ProgrammingFundamentalsExercise2/Exercise2.java
@@ -0,0 +1,28 @@
+package ProgrammingFundamentalsExercise2;
+
+import java.io.Console;
+
+public class Exercise2 {
+    
+    public static void main(String[] args) {
+        Console console = System.console();
+        /*  Some terms:
+            noun - Person, place or thing
+            verb - An action
+            adjective - A description used to modify or describe a noun
+            Enter your amazing code here!
+        */
+        //__Name__ is a __adjective__ __noun__.  They are always __adverb__ __verb__.
+
+        String name = console.readLine("Enter your name: ");
+        String adjective = console.readLine("Enter an adjective: ");
+        String noun = console.readLine("Enter a noun: ");
+        String adverb = console.readLine("Enter an adverb: ");
+        String verb = console.readLine("Enter a verb: ");
+
+        console.printf("Your TreeStory:\n---------------------\n");
+        console.printf("%s is a %s %s. ", name, adjective, noun);
+        console.printf("They are always %s %s.\n", adverb, verb);
+    }
+    
+}
