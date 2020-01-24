aaeb406dc2115adb662f09c3f7a69763b2cfd7fb
Marlo Baguyos <Marlo.Baguyos@microsoft.com>
2020-01-24 20:58:38 +0000

    finish exercise2

--- ProgrammingFundamentalsExercise2/Exercise2.java
+++ ProgrammingFundamentalsExercise2/Exercise2.java
@@ -9,18 +9,38 @@ public class Exercise2 {
     public static void main(String[] args) {
         sc = new Scanner(System.in);
         
-        System.out.println("What is your name: ");
-        String name = sc.nextLine();
-            if (name == " ") {
+        // System.out.println("What is your name: ");
+        // String name = sc.nextLine();
+        String name;
+        boolean noName;
+            do {
+                System.out.println("What is your name? ");
+                name = sc.nextLine();
+                noName = name.isEmpty();
+                if (noName) {
                 System.out.println("You did not print anything.  Please Try again");
+                } 
+            } while (noName);
+            
+        int age;
+        System.out.println("How old are you? ");
+            while (!sc.hasNextInt()) {
+                System.out.println("Invalid Input:  Use a whole number DUMMY!");
+                System.out.println("Lets try again.  How old are you?  ");
+                sc.nextLine();
             }
-        System.out.println("What is your age: ");
-        int age = Integer.parseInt(sc.nextLine());
+            age = sc.nextInt();
 
         System.out.println("What is your current salary? ");
-        double salary = Double.parseDouble(sc.nextLine());
+        double salary;
+            while (!sc.hasNextDouble()){
+                System.out.println("Invalid Input:  Use a whole number DUMMY!");
+                System.out.println("Lets try again.  Dummy, how much do you make?  ");
+                sc.nextLine();
+            }
+            salary = sc.nextDouble();
 
-        String ballerStatus = name + " is " + age + " years of age making $" + salary + " .  ***Baller Status***.";
+        String ballerStatus = name + " is " + age + " years of age making $" + salary + " .  ***Baller Status***.\n";
         System.out.print(ballerStatus);
     }
 }
