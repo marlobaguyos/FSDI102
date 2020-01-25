575806a08480e6f14c55551a7c81f3f10b795a4d
Marlo Baguyos <Marlo.Baguyos@microsoft.com>
2020-01-25 22:36:48 +0000

    Adding examples

---
+++ Practice/JavaObjects/Example.java
@@ -0,0 +1,10 @@
+package Practice.JavaObjects;
+
+public class Example {
+	public static void main (String[] args) {
+		System.out.println("We are making a new PEZ Dispenser");
+		//instantiate a new Pezdispenser object and it creates a new instance.  The dispenser is referred to as an instance of type  Pezdispenser.
+		PezDispenser dispenser = new PezDispenser();
+		System.out.printf("The dispenser is %s %n", dispenser.characterName);
+	}
+}
---
+++ Practice/JavaObjects/PezDispenser.java
@@ -0,0 +1,6 @@
+package Practice.JavaObjects;
+
+//Anything between { } is known as class scopre
+class PezDispenser {
+	String characterName = "Yoda";
+}
