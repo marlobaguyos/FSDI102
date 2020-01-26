74d0fa3f4e28df2ef52c6dad7af51e280aa0ab1d
Marlo Baguyos <Marlo.Baguyos@microsoft.com>
2020-01-26 19:33:21 +0000

    update Example.java

--- Sessions/Example.java
+++ Sessions/Example.java
@@ -6,6 +6,7 @@ public class Example {
 		Student s1 = new Student("Efren", 29, 7);
 		Student s2 = new Student("Bob", 30, 7);
 		Student s3 = new Student("Jake", 22, 8);
+		Student s4 = new Student("Lukas", 50, 9);
 
 		User u1 = new User("Andres", 34);
 		Animal a1 = new Animal("Machiato", "Dog");
@@ -17,6 +18,7 @@ public class Example {
 		System.out.println(u1.name);
 		System.out.println(a1.name);
 
+		a1.attack(s4);
 		a2.attack(s1);
 		a3.attack(s2);
 		a4.attack(s3);
