b1e06b2643ea4096114300b4665f19d0f381e6b5
Marlo Baguyos <Marlo.Baguyos@microsoft.com>
2020-01-26 13:48:16 +0000

    update

--- Practice/JavaObjects/Example.java
+++ Practice/JavaObjects/Example.java
@@ -2,9 +2,17 @@ package Practice.JavaObjects;
 
 public class Example {
 	public static void main (String[] args) {
+
 		System.out.println("We are making a new PEZ Dispenser");
+
+		System.out.printf("Fun FACT: There are %d PEZ allowed in every dispenser %n", PezDispenser.MAX_PEZ);
+
 		//instantiate a new Pezdispenser object and it creates a new instance.  The dispenser is referred to as an instance of type  Pezdispenser.
 		PezDispenser dispenser = new PezDispenser("Yoda");
+
 		System.out.printf("The dispenser is %s %n", dispenser.getCharacterName());
+
+		System.out.printf("Filling the dispenser with delicious PEZ... %n");
+		dispenser.fill();
 	}
 }
--- Practice/JavaObjects/PezDispenser.java
+++ Practice/JavaObjects/PezDispenser.java
@@ -3,11 +3,17 @@ package Practice.JavaObjects;
 //Anything between { } is known as class scopre
 class PezDispenser {
 	//constant by making all CAPs
-	final private int MAX_PEZ = 12;
+	public static final int MAX_PEZ = 12;
 	final private String characterName;
+	private int pezCount;
 
 	public PezDispenser(String characterName) {
 		this.characterName = characterName;
+		pezCount = 0;
+	}
+
+	public void fill() {
+		pezCount = MAX_PEZ;
 	}
 
 	public String getCharacterName() {
