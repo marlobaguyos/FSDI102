b23728c2755340dc6b9724f9cb57601140b79f29
Marlo Baguyos <Marlo.Baguyos@microsoft.com>
2020-01-26 04:56:37 +0000

    update example and PezDispenser files

--- Practice/JavaObjects/Example.java
+++ Practice/JavaObjects/Example.java
@@ -4,8 +4,7 @@ public class Example {
 	public static void main (String[] args) {
 		System.out.println("We are making a new PEZ Dispenser");
 		//instantiate a new Pezdispenser object and it creates a new instance.  The dispenser is referred to as an instance of type  Pezdispenser.
-		PezDispenser dispenser = new PezDispenser();
-		dispenser.characterName = "Darth Vader";
-		System.out.printf("The dispenser is %s %n", dispenser.characterName);
+		PezDispenser dispenser = new PezDispenser("Yoda");
+		System.out.printf("The dispenser is %s %n", dispenser.getCharacterName());
 	}
 }
--- Practice/JavaObjects/PezDispenser.java
+++ Practice/JavaObjects/PezDispenser.java
@@ -2,5 +2,16 @@ package Practice.JavaObjects;
 
 //Anything between { } is known as class scopre
 class PezDispenser {
-	String characterName = "Yoda";
+	//constant by making all CAPs
+	final private int MAX_PEZ = 12;
+	final private String characterName;
+
+	public PezDispenser(String characterName) {
+		this.characterName = characterName;
+	}
+
+	public String getCharacterName() {
+		return characterName;
+	}
 }
+
