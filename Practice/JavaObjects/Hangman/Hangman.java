24bc530fdc9bdcc2762595964be042758ef4e6a3
Marlo Baguyos <Marlo.Baguyos@microsoft.com>
2020-01-26 23:12:41 +0000

    hangman update

---
+++ Practice/JavaObjects/Hangman/Hangman.java
@@ -0,0 +1,15 @@
+package Practice.JavaObjects.Hangman;
+
+public class Hangman {
+
+	public static void main(String args[]) {
+		Game game = new Game("treehouse");
+		Prompter prompter = new Prompter(game);
+		boolean isHit = prompter.promptForGuess();		
+		if (isHit) {
+			System.out.println("We got a hit!");
+		} else {
+			System.out.println("Oops missed");
+		}
+	}
+}
